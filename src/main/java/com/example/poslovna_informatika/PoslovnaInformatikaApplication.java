3d6fa0ca0d1eb7ea8d36540fb877aac952022682
Marko Jokic <markojokic1996@gmail.com>
2018-02-24 15:39:36 +0100

    Hello world!

---
+++ src/main/java/com/example/poslovna_informatika/PoslovnaInformatikaApplication.java
@@ -0,0 +1,12 @@
+package com.example.poslovna_informatika;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PoslovnaInformatikaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PoslovnaInformatikaApplication.class, args);
+    }
+}
---
+++ src/test/java/com/example/poslovna_informatika/PoslovnaInformatikaApplicationTests.java
@@ -0,0 +1,16 @@
+package com.example.poslovna_informatika;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class PoslovnaInformatikaApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
