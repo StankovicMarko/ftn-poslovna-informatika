b12a22ad7bd539be097aeea4aeeb5f456e782977
Marko Jokic <markojokic1996@gmail.com>
2018-03-23 20:30:32 +0100

    Services example

---
+++ src/main/java/com/example/poslovna_informatika/serviceInterfaces/PreduzeceServiceInterface.java
@@ -0,0 +1,25 @@
+package com.example.poslovna_informatika.serviceInterfaces;
+
+import com.example.poslovna_informatika.model.Preduzece;
+
+import java.util.List;
+
+public interface PreduzeceServiceInterface {
+
+    List<Preduzece> findAll();
+
+    Preduzece findByNaziv(String naziv);
+
+    Preduzece findByPib(int pib);
+
+    List<Preduzece> findAllByAdresa(String adresa);
+
+    List<Preduzece> findAllByTelefon(String telefon);
+
+    List<Preduzece> findAllByEmail(String email);
+
+    List<Preduzece> findAllByMestoId(long mestoId);
+
+    Preduzece save(Preduzece preduzece);
+
+}
---
+++ src/main/java/com/example/poslovna_informatika/services/PreduzeceService.java
@@ -0,0 +1,60 @@
+package com.example.poslovna_informatika.services;
+
+import com.example.poslovna_informatika.model.Preduzece;
+import com.example.poslovna_informatika.repositories.PreduzeceRepository;
+import com.example.poslovna_informatika.serviceInterfaces.PreduzeceServiceInterface;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class PreduzeceService implements PreduzeceServiceInterface {
+
+    private PreduzeceRepository preduzeceRepository;
+
+    @Autowired
+    public PreduzeceService(PreduzeceRepository preduzeceRepository) {
+        this.preduzeceRepository = preduzeceRepository;
+    }
+
+    @Override
+    public List<Preduzece> findAll() {
+        return preduzeceRepository.findAll();
+    }
+
+    @Override
+    public Preduzece findByNaziv(String naziv) {
+        return preduzeceRepository.findByNaziv(naziv);
+    }
+
+    @Override
+    public Preduzece findByPib(int pib) {
+        return preduzeceRepository.findByPib(pib);
+    }
+
+    @Override
+    public List<Preduzece> findAllByAdresa(String adresa) {
+        return preduzeceRepository.findAllByAdresa(adresa);
+    }
+
+    @Override
+    public List<Preduzece> findAllByTelefon(String telefon) {
+        return preduzeceRepository.findAllByTelefon(telefon);
+    }
+
+    @Override
+    public List<Preduzece> findAllByEmail(String email) {
+        return preduzeceRepository.findAllByEmail(email);
+    }
+
+    @Override
+    public List<Preduzece> findAllByMestoId(long mestoId) {
+        return preduzeceRepository.findAllByMestoId(mestoId);
+    }
+
+    @Override
+    public Preduzece save(Preduzece preduzece) {
+        return preduzeceRepository.save(preduzece);
+    }
+}
