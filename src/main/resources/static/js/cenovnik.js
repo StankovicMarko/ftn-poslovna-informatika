8c53d26653a9f5d50f69317678735ed5282280c4
Marko Stankovic <stankovic.programming@outlook.com>
2018-04-14 19:45:10 +0200

    cenovnik in progress

--- src/main/resources/static/js/cenovnik.js
+++ src/main/resources/static/js/cenovnik.js
@@ -1,15 +1,26 @@
+
+    var preduzeceId;
     var sviCenovnici;
+    var svaPreduzeca;
+
 
 $(document).ready(function () {
+     loadPreduzeca();
 
-    loadCenovnici();
+
+
+
+    //loadCenovnici(preduzeceId);
+
 
 });
 
-function loadCenovnici() {
+
+
+function loadCenovnici(preduzeceId) {
     $.ajax({
         type: "GET",
-        url: "api/cenovnik",
+        url: "api/cenovnik/"+preduzeceId,
         dataType: "json",
         success: function (cenovnik) {
                 sviCenovnici=cenovnik;
@@ -17,31 +28,54 @@ function loadCenovnici() {
                     $('#cenovnik').append('<tr> <td>' + cenovnik.id+ '</td> <td>'+cenovnik.datumVazenja+'</td> </tr>');
                 });
             }
+    });
+}
+
+function loadPreduzeca() {
+    $.ajax({
+        type: "GET",
+        url: "api/preduzece",
+        dataType: "json",
+        success: function (preduzeca) {
+                svaPreduzeca=preduzeca;
+                preduzeca.forEach(function (preduzece) {
+                    $('#lista-preduzeca').append('<option>'+preduzece.id+'. '+preduzece.naziv+'</option>');
+                });
+            }
 
     });
 }
 
+$('#lista-preduzeca').on('change', function() {
 
+     var preduzeceIdString = $(this).find(":selected").text();
+     preduzeceId = preduzeceIdString.substr(0, preduzeceIdString.indexOf('.'));
+      loadCenovnici(preduzeceId);
+
+
+});
 
 $('#cenovnik-add-form').submit(function (e) {
     e.preventDefault();
 
-    var grad = $('#mesto-grad-add').val();
-    var drzava = $('#mesto-drzava-add').val();
+
+    var datumVazenja = $('#cenovnik-datumVazenja-add').val();
 
     var data = {
-        "grad": grad,
-        "drzava": drzava
+        "datumVazenja": datumVazenja,
+        "preduzeceId" : preduzeceId
     };
+
+    console.log(data);
 
     $.ajax({
         type: "POST",
-        url: "api/mesto",
+        url: "api/cenovnik",
         data: JSON.stringify(data),
         contentType: "application/json",
         success: function (response) {
         console.log(response);
-            $('#add-mesto').modal('toggle');
+            $('#add-cenovnik').modal('toggle');
             location.reload(true); //reloads from server rather than browser cache
 //            alert(response['message']);
         },
@@ -53,52 +87,48 @@ $('#cenovnik-add-form').submit(function (e) {
 
 });
 
-$('#mesta').on( 'click', 'tr', function () {
-    var mestoId = $(this).children(':first').text();
-    $('#edit-mesto').modal('toggle');
+$('#cenovnik').on( 'click', 'tr', function () {
+    var cenovnikId = $(this).children(':first').text();
+    $('#edit-cenovnik').modal('toggle');
 
-    var mesto = svaMesta.find(function(element) {
-                           return element.id == mestoId;
+    var cenovnik = sviCenovnici.find(function(element) {
+                           return element.id == cenovnikId;
                          });
 
 
-    var grad = $('#mesto-grad-edit').val(mesto.grad);
-    var drzava = $('#mesto-drzava-edit').val(mesto.drzava);
-    $("#preduzeca").empty();
+    var datumVazenja = $('#cenovnik-datumVazenja-edit').val(cenovnik.datumVazenja);
+    $("#stavkeCenovnika").empty();
 
      $.ajax({
       type: "GET",
-      url: "api/preduzece/mesto/"+mestoId,
+      url: "api/stavka-cenovnika/cenovnik/"+cenovnikId,
       dataType: "json",
-      success: function (preduzeca) {
-             preduzeca.forEach(function (pred) {
-                                     $('#preduzeca').append('<tr> <td>'+pred.naziv+'</td> <td>'+pred.adresa+'</td> <td> ' +pred.pib + '</td> </tr>');
+      success: function (cenovnika) {
+             cenovnika.forEach(function (cenovnik) {
+                                     $('#stavkeCenovnika').append('<tr> <td>'+cenovnik.nazivRobe+'</td> <td>'+cenovnik.cena+'</td> </tr>');
                                  });
           }});
 
 
-
-
-
     // mogucnost submita menjanja podatak i delete brisanja
 
-    $('#mesto-edit-form').submit(function (e) {
+    $('#cenovnik-edit-form').submit(function (e) {
         e.preventDefault();
 
 
         var data = {
-            "grad": grad.val(),
-            "drzava": drzava.val()
+            "datumVazenja": datumVazenja.val(),
+            "preduzeceId": preduzeceId
         };
 
         $.ajax({
             type: "PUT",
-            url: "api/mesto/"+mestoId,
+            url: "api/cenovnik/"+cenovnikId,
             data: JSON.stringify(data),
             contentType: "application/json",
             success: function (response) {
             console.log(response);
-                $('#edit-mesto').modal('toggle');
+                $('#edit-cenovnik').modal('toggle');
                 location.reload(true); //reloads from server rather than browser cache
     //            alert(response['message']);
             },
@@ -111,19 +141,19 @@ $('#mesta').on( 'click', 'tr', function () {
     });
 
 
-    $('#mesto-edit-form').on( 'click', '.btn-danger', function (e){
+    $('#cenovnik-edit-form').on( 'click', '.btn-danger', function (e){
     e.preventDefault();
 
-     if (confirm('Are you sure you want do delete this Mesto?')) {
+     if (confirm('Are you sure you want do delete this Cenovnik?')) {
             $.ajax({
                 type: 'DELETE',
-                url: 'api/mesto/' + mestoId,
+                url: 'api/cenovnik/' + cenovnikId,
                 contentType: "application/json",
                 success: function (response) {
                 location.reload(true); //reloads from server rather than browser cache
                 },
                 error: function (err) {
-                    alert("Can't delete Mesto that have Preduzeca");
+                    alert("Can't delete Cenovnik that have Stavke Cenovnika");
                 }
             });
         }
