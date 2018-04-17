626181eef44361b7a15fa5493b7528b8781d71b7
Marko Jokic <markojokic1996@gmail.com>
2018-04-17 20:11:53 +0200

    Extracted faktura logic from controllers

--- src/main/java/com/example/poslovna_informatika/controllers/FakturaController.java
+++ src/main/java/com/example/poslovna_informatika/controllers/FakturaController.java
@@ -1,16 +1,12 @@
 package com.example.poslovna_informatika.controllers;
 
 import com.example.poslovna_informatika.dto.FakturaDTO;
-import com.example.poslovna_informatika.model.*;
-import com.example.poslovna_informatika.services.*;
+import com.example.poslovna_informatika.services.FakturaService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import static java.lang.Math.toIntExact;
 
-
-import java.util.ArrayList;
 import java.util.List;
 
 @RestController
@@ -18,108 +14,42 @@ import java.util.List;
 public class FakturaController {
 
     private FakturaService fakturaService;
-    private PreduzeceService preduzeceService;
-    private PoslovnaGodinaService poslovnaGodinaService;
-    private PoslovniPartnerService poslovniPartnerService;
-    private StavkaFaktureService stavkaFaktureService;
-
 
     @Autowired
-    public FakturaController(FakturaService fakturaService, PreduzeceService preduzeceService,
-                             PoslovnaGodinaService poslovnaGodinaService, PoslovniPartnerService poslovniPartnerService,
-                             StavkaFaktureService stavkaFaktureService) {
+    public FakturaController(FakturaService fakturaService) {
         this.fakturaService = fakturaService;
-        this.preduzeceService = preduzeceService;
-        this.poslovnaGodinaService = poslovnaGodinaService;
-        this.poslovniPartnerService = poslovniPartnerService;
-        this.stavkaFaktureService = stavkaFaktureService;
     }
-
-
-
 
 
     @GetMapping(value = "/{id}")
     public ResponseEntity<List<FakturaDTO>> getFakture(@PathVariable("id") long id) {
-        List<Faktura> fakture = fakturaService.findAllByPreduzeceId(id);
-        List<FakturaDTO> fakturaDTOS = new ArrayList<FakturaDTO>();
-        for (Faktura f : fakture) {
-            fakturaDTOS.add(new FakturaDTO(f));
-        }
-        return new ResponseEntity<List<FakturaDTO>>(fakturaDTOS, HttpStatus.OK);
+        return new ResponseEntity<>(fakturaService.getFaktureByPreduzeceId(id), HttpStatus.OK);
     }
 
     @GetMapping(value = "/partner/{id}")
     public ResponseEntity<List<FakturaDTO>> getPartnerFakture(@PathVariable("id") long id) {
-        List<Faktura> fakture = fakturaService.findAllByPoslovniPartnerId(id);
-        List<FakturaDTO> fakturaDTOS = new ArrayList<FakturaDTO>();
-        for (Faktura f : fakture) {
-            fakturaDTOS.add(new FakturaDTO(f));
-        }
-        return new ResponseEntity<List<FakturaDTO>>(fakturaDTOS, HttpStatus.OK);
+        return new ResponseEntity<>(fakturaService.getFaktureByPartnerId(id), HttpStatus.OK);
     }
 
     @PostMapping(consumes = "application/json")
     public ResponseEntity<FakturaDTO> saveItem(@RequestBody FakturaDTO fakturaDTO) {
-        Preduzece preduzece = preduzeceService.findOne(fakturaDTO.getPreduzeceId());
-        PoslovnaGodina poslovnaGodina = poslovnaGodinaService.findOne(fakturaDTO.getPoslovnaGodinaId());
-        PoslovniPartner poslovniPartner = poslovniPartnerService.findOne(fakturaDTO.getPoslovniPartnerId());
-
-        Faktura f = new Faktura(preduzece, poslovniPartner, poslovnaGodina, fakturaDTO.getStatus());
-
-        f = fakturaService.save(f);
-
-        f.setBrojFakture(toIntExact(f.getId()));
-        f = fakturaService.save(f);
-
-        return new ResponseEntity<FakturaDTO>(new FakturaDTO(f), HttpStatus.CREATED);
-
+        return new ResponseEntity<>(fakturaService.saveFakturaDTO(fakturaDTO), HttpStatus.CREATED);
     }
 
 
     @PutMapping(value = "/{id}", consumes = "application/json")
     public ResponseEntity<FakturaDTO> updateItem(@RequestBody FakturaDTO fakturaDTO, @PathVariable("id") long id) {
-        Faktura f = fakturaService.findOne(id);
+        FakturaDTO f = fakturaService.updateFakturaDTO(fakturaDTO, id);
 
-        if (f == null) {
-            return new ResponseEntity<FakturaDTO>(HttpStatus.BAD_REQUEST);
-        }
+        if (f == null) return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
 
-        Preduzece preduzece = preduzeceService.findOne(fakturaDTO.getPreduzeceId());
-        PoslovnaGodina poslovnaGodina = poslovnaGodinaService.findOne(fakturaDTO.getPoslovnaGodinaId());
-        PoslovniPartner poslovniPartner = poslovniPartnerService.findOne(fakturaDTO.getPoslovniPartnerId());
-
-        f.setDatumFakture(fakturaDTO.getDatumFakture());
-        f.setDatumValute(fakturaDTO.getDatumValute());
-        f.setOsnovica(fakturaDTO.getOsnovica());
-        f.setUkupanPDV(fakturaDTO.getUkupanPdv());
-        f.setIznosZaPlacanje(fakturaDTO.getIznosZaPlacanje());
-        f.setStatus(fakturaDTO.getStatus());
-        f.setPreduzece(preduzece);
-        f.setPoslovniPartner(poslovniPartner);
-        f.setPoslovnaGodina(poslovnaGodina);
-
-
-        f = fakturaService.save(f);
-
-        return new ResponseEntity<FakturaDTO>(new FakturaDTO(f), HttpStatus.OK);
+        return new ResponseEntity<>(f, HttpStatus.OK);
     }
 
 
     @DeleteMapping(value = "/{id}")
     public ResponseEntity<Void> deleteItem(@PathVariable long id) {
-        Faktura f = fakturaService.findOne(id);
-        List<StavkaFakture> stavkaFaktures = stavkaFaktureService.findAllByFakturaId(id);
-        for (StavkaFakture st: stavkaFaktures) {
-            stavkaFaktureService.remove(st.getId());
-            
-        }
-
-        if (f != null) {
-            fakturaService.remove(id);
-            return new ResponseEntity<Void>(HttpStatus.OK);
-        } else {
-            return new ResponseEntity<Void>(HttpStatus.NOT_FOUND);
-        }
+        if (fakturaService.deleteFaktura(id)) return new ResponseEntity<>(HttpStatus.OK);
+        return new ResponseEntity<>(HttpStatus.NOT_FOUND);
     }
 }
--- src/main/java/com/example/poslovna_informatika/services/FakturaService.java
+++ src/main/java/com/example/poslovna_informatika/services/FakturaService.java
@@ -1,22 +1,36 @@
 package com.example.poslovna_informatika.services;
 
-import com.example.poslovna_informatika.model.Faktura;
+import com.example.poslovna_informatika.dto.FakturaDTO;
+import com.example.poslovna_informatika.model.*;
 import com.example.poslovna_informatika.repositories.FakturaRepository;
 import com.example.poslovna_informatika.serviceInterfaces.FakturaServiceInterface;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
+
+import static java.lang.Math.toIntExact;
 
 @Service
 public class FakturaService implements FakturaServiceInterface {
 
     private FakturaRepository fakturaRepository;
+    private PreduzeceService preduzeceService;
+    private PoslovnaGodinaService poslovnaGodinaService;
+    private PoslovniPartnerService poslovniPartnerService;
+    private StavkaFaktureService stavkaFaktureService;
 
     @Autowired
-    public FakturaService(FakturaRepository fakturaRepository) {
+    public FakturaService(FakturaRepository fakturaRepository, PreduzeceService preduzeceService,
+                          PoslovnaGodinaService poslovnaGodinaService,
+                          PoslovniPartnerService poslovniPartnerService, StavkaFaktureService stavkaFaktureService) {
         this.fakturaRepository = fakturaRepository;
+        this.preduzeceService = preduzeceService;
+        this.poslovnaGodinaService = poslovnaGodinaService;
+        this.poslovniPartnerService = poslovniPartnerService;
+        this.stavkaFaktureService = stavkaFaktureService;
     }
 
     @Override
@@ -26,7 +40,7 @@ public class FakturaService implements FakturaServiceInterface {
     }
 
     @Override
-    public Faktura findOne(long id){
+    public Faktura findOne(long id) {
         return fakturaRepository.findOne(id);
     }
 
@@ -88,6 +102,80 @@ public class FakturaService implements FakturaServiceInterface {
 
     @Override
     public void remove(long id) {
-         fakturaRepository.delete(id);
+        fakturaRepository.delete(id);
+    }
+
+
+    public List<FakturaDTO> getFaktureByPreduzeceId(long id) {
+        List<Faktura> fakture = findAllByPreduzeceId(id);
+        List<FakturaDTO> fakturaDTOS = new ArrayList<>();
+        for (Faktura f : fakture) {
+            fakturaDTOS.add(new FakturaDTO(f));
+        }
+        return fakturaDTOS;
+    }
+
+    public List<FakturaDTO> getFaktureByPartnerId(long id) {
+        List<Faktura> fakture = findAllByPoslovniPartnerId(id);
+        List<FakturaDTO> fakturaDTOS = new ArrayList<>();
+        for (Faktura f : fakture) {
+            fakturaDTOS.add(new FakturaDTO(f));
+        }
+        return fakturaDTOS;
+    }
+
+    public FakturaDTO saveFakturaDTO(FakturaDTO fakturaDTO) {
+        Preduzece preduzece = preduzeceService.findOne(fakturaDTO.getPreduzeceId());
+        PoslovnaGodina poslovnaGodina = poslovnaGodinaService.findOne(fakturaDTO.getPoslovnaGodinaId());
+        PoslovniPartner poslovniPartner = poslovniPartnerService.findOne(fakturaDTO.getPoslovniPartnerId());
+
+        Faktura f = new Faktura(preduzece, poslovniPartner, poslovnaGodina, fakturaDTO.getStatus());
+
+        f = save(f);
+
+        f.setBrojFakture(toIntExact(f.getId()));
+        f = save(f);
+
+        return new FakturaDTO(f);
+    }
+
+    public FakturaDTO updateFakturaDTO(FakturaDTO fakturaDTO, long id) {
+        Faktura f = findOne(id);
+
+        if (f == null) {
+            return null;
+        }
+
+        Preduzece preduzece = preduzeceService.findOne(fakturaDTO.getPreduzeceId());
+        PoslovnaGodina poslovnaGodina = poslovnaGodinaService.findOne(fakturaDTO.getPoslovnaGodinaId());
+        PoslovniPartner poslovniPartner = poslovniPartnerService.findOne(fakturaDTO.getPoslovniPartnerId());
+
+        f.setDatumFakture(fakturaDTO.getDatumFakture());
+        f.setDatumValute(fakturaDTO.getDatumValute());
+        f.setOsnovica(fakturaDTO.getOsnovica());
+        f.setUkupanPDV(fakturaDTO.getUkupanPdv());
+        f.setIznosZaPlacanje(fakturaDTO.getIznosZaPlacanje());
+        f.setStatus(fakturaDTO.getStatus());
+        f.setPreduzece(preduzece);
+        f.setPoslovniPartner(poslovniPartner);
+        f.setPoslovnaGodina(poslovnaGodina);
+
+        f = save(f);
+
+        return new FakturaDTO(f);
+    }
+
+    public boolean deleteFaktura(long id) {
+        Faktura f = findOne(id);
+        List<StavkaFakture> stavkaFaktures = stavkaFaktureService.findAllByFakturaId(id);
+        for (StavkaFakture st : stavkaFaktures) {
+            stavkaFaktureService.remove(st.getId());
+        }
+
+        if (f != null) {
+            remove(id);
+            return true;
+        }
+        return false;
     }
 }
